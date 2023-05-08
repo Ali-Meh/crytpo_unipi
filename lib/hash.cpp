7ed46dcf93dcf5f03b3bf3a501833df7a05ce630
Ali Mahdavi <a5mahdavy5@gmail.com>
2023-05-08 21:07:38 +0000

    hash and verify password

--- lib/hash.cpp
+++ lib/hash.cpp
@@ -3,87 +3,13 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <vector>
+#include <sstream>
 #include "util.cpp"
 
 using namespace std;
 
 const int SALT_SIZE = 16;
-
-// class Password
-// {
-// public:
-//     Password() : salt_size(16) {}
-//     Password(const string &password, int salt_size = 16) : salt_size(salt_size)
-//     {
-//         salt = generate_salt();
-//         hash_password(password);
-//     }
-
-//     void hash_password(const string &password)
-//     {
-//         EVP_MD_CTX *ctx = EVP_MD_CTX_new();
-//         const EVP_MD *md = EVP_sha256();
-//         unsigned char digest[EVP_MAX_MD_SIZE];
-//         unsigned int digest_len;
-
-//         // Hash the password with the salt
-//         EVP_DigestInit_ex(ctx, md, NULL);
-//         EVP_DigestUpdate(ctx, salt.data(), salt_size);
-//         EVP_DigestUpdate(ctx, password.data(), password.size());
-//         EVP_DigestFinal_ex(ctx, digest, &digest_len);
-
-//         // Store the hashed password
-//         hashed_password.assign((const char *)digest, digest_len);
-//         EVP_MD_CTX_free(ctx);
-//     }
-
-//     bool verify_password(const string &password) const
-//     {
-//         EVP_MD_CTX *context = EVP_MD_CTX_new();
-//         const EVP_MD *md = EVP_sha256();
-//         unsigned char digest[EVP_MAX_MD_SIZE];
-//         unsigned int digest_len;
-
-//         // Hash the password with the salt
-//         EVP_DigestInit_ex(context, md, NULL);
-//         EVP_DigestUpdate(context, salt.data(), salt_size);
-//         EVP_DigestUpdate(context, password.data(), password.size());
-//         EVP_DigestFinal_ex(context, digest, &digest_len);
-
-//         // Compare the hashed password with the stored hash
-//         bool result = (hashed_password == string((const char *)digest, digest_len));
-//         EVP_MD_CTX_free(context);
-//         return result;
-//     }
-
-//     string get_hashed_password() const
-//     {
-//         return hashed_password;
-//     }
-
-//     string get_salt() const
-//     {
-//         return salt;
-//     }
-
-//     // Override string to embed salt
-//     operator string() const
-//     {
-//         return salt + ":" + hashed_password;
-//     }
-
-// private:
-//     string generate_salt()
-//     {
-//         string salt(salt_size, 0);
-//         RAND_bytes((unsigned char *)salt.data(), salt_size);
-//         return salt;
-//     }
-
-//     string hashed_password;
-//     string salt;
-//     int salt_size;
-// };
 
 string generate_salt(int salt_size = SALT_SIZE)
 {
@@ -111,24 +37,61 @@ string hash_password(const string &password)
     hashed_password = std::string((const char *)digest, digest_len);
 
     EVP_MD_CTX_free(ctx);
-    return string_to_hex(salt) + ":" + string_to_hex(hashed_password);
+    return salt + ":" + hashed_password;
+}
+
+bool verify_password(const string &input, const string &current)
+{
+    EVP_MD_CTX *context = EVP_MD_CTX_new();
+    const EVP_MD *md = EVP_sha256();
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int digest_len;
+    vector<string> splitted = split(current, ':');
+    string salt = splitted[0];
+    string hashed_password = splitted[1];
+
+    // Hash the password with the salt
+    EVP_DigestInit_ex(context, md, NULL);
+    EVP_DigestUpdate(context, salt.data(), salt.size());
+    EVP_DigestUpdate(context, input.data(), input.size());
+    EVP_DigestFinal_ex(context, digest, &digest_len);
+
+    // Compare the hashed password with the stored hash
+    bool result = (hashed_password == std::string((const char *)digest, digest_len));
+    EVP_MD_CTX_free(context);
+    return result;
 }
 
 int main()
 {
-    // Password p("password123");
-    // string salt = p.get_salt();
-    string hashed_password = hash_password("password1231");
-    vector<string> splited = split(hashed_password, ":");
-    cout << "=> " << hashed_password << endl;
-    cout << "Salt=> " << splited[0] << endl;
-    cout << "Hashed password=> " << splited[1] << endl;
+    // Hash a password and store it
+    string hashed_password = hash_password("password1123");
 
-    // // Verify the password
-    // bool result = p.verify_password("password123");
-    // cout << "Password verification result: " << result << endl;
+    // Split the hashed password into salt and hash
+    vector<string> splitted = split(hashed_password, ':');
+    string salt = splitted[0];
+    string stored_hash = splitted[1];
 
-    // // Override string to embed salt
-    // string password_str = p;
-    // cout << "Password string: " << password_str << endl;
+    // Verify a password
+    // string input_password;
+    // cout << "Enter password: ";
+    // cin >> input_password;
+
+    cout << "password hash=> " << hashed_password << endl;
+    bool r = verify_password("input_password", hashed_password);
+    cout << r << endl;
+
+    r = verify_password("password1123", hashed_password);
+    cout << r << endl;
+
+    // if (verify_password(input_password, hashed_password))
+    // {
+    //     cout << "Password is correct!" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Incorrect password." << endl;
+    // }
+
+    return 0;
 }
--- lib/util.cpp
+++ lib/util.cpp
@@ -1,92 +1,126 @@
-#include <iostream>
-#include <string>
 #include <vector>
+#include <sstream>
+#include <iostream>
+
 using namespace std;
 
 // create custom split() function
-vector<string> split(string str, string del)
+vector<string> split(string str, char del)
 {
-    vector<string> s;
-    // Use find function to find 1st position of delimiter.
-    int end = str.find(del);
-    while (end != -1)
-    { // Loop until no delimiter is left in the string.
-        s.push_back(str.substr(0, end));
-        str.erase(str.begin(), str.begin() + end + 1);
-        end = str.find(del);
-    }
-    s.push_back(str.substr(0, end));
-
-    return s;
-}
-
-#include <string>
-#include <sstream>
-
-// Convert a string to hex
-std::string string_to_hex(const std::string &input)
-{
-    static const char *const lut = "0123456789abcdef";
-    size_t len = input.length();
-
-    std::string output;
-    output.reserve(2 * len);
-    for (size_t i = 0; i < len; ++i)
+    vector<string> splitted;
+    stringstream ss(str);
+    string item;
+    while (getline(ss, item, del))
     {
-        const unsigned char c = input[i];
-        output.push_back(lut[c >> 4]);
-        output.push_back(lut[c & 15]);
+        splitted.push_back(item);
     }
-    return output;
+
+    return splitted;
 }
 
-// Convert hex to a string
-std::string hex_to_string(const std::string &input)
-{
-    static const char *const lut = "0123456789abcdef";
-    size_t len = input.length();
+// // Convert a string to hex
+// std::string string_to_hex(const std::string &input)
+// {
+//     static const char *const lut = "0123456789abcdef";
+//     size_t len = input.length();
 
-    if (len & 1)
-        throw std::invalid_argument("Odd length");
+//     std::string output;
+//     output.reserve(2 * len);
+//     for (size_t i = 0; i < len; ++i)
+//     {
+//         const unsigned char c = input[i];
+//         output.push_back(lut[c >> 4]);
+//         output.push_back(lut[c & 15]);
+//     }
+//     return output;
+// }
 
-    std::string output;
-    output.reserve(len / 2);
-    for (size_t i = 0; i < len; i += 2)
-    {
-        char a = input[i];
-        const char *p = std::lower_bound(lut, lut + 16, a);
-        if (*p != a)
-            throw std::invalid_argument("Not a hex digit");
+// // Convert hex to a string
+// std::string hex_to_string(const std::string &input)
+// {
+//     static const char *const lut = "0123456789abcdef";
+//     size_t len = input.length();
 
-        char b = input[i + 1];
-        const char *q = std::lower_bound(lut, lut + 16, b);
-        if (*q != b)
-            throw std::invalid_argument("Not a hex digit");
+//     if (len & 1)
+//         throw std::invalid_argument("Odd length");
 
-        output.push_back(((p - lut) << 4) | (q - lut));
-    }
-    return output;
-}
+//     std::string output;
+//     output.reserve(len / 2);
+//     for (size_t i = 0; i < len; i += 2)
+//     {
+//         char a = input[i];
+//         const char *p = std::lower_bound(lut, lut + 16, a);
+//         if (*p != a)
+//             throw std::invalid_argument("Not a hex digit");
 
-// uses overloaded '=' operator from string class
-// to convert character array to string
-string convertToString(char *a)
-{
-    string s = a;
-    return s;
-}
+//         char b = input[i + 1];
+//         const char *q = std::lower_bound(lut, lut + 16, b);
+//         if (*q != b)
+//             throw std::invalid_argument("Not a hex digit");
 
-inline char binary_to_hex_digit(unsigned a)
-{
-    return a + (a < 10 ? '0' : 'a' - 10);
-}
-std::string binary_to_hex(unsigned char const *binary, unsigned binary_len)
-{
-    std::string r(binary_len * 2, '\0');
-    for (unsigned i = 0; i < binary_len; ++i)
-    {
-        r[i * 2] = binary_to_hex_digit(binary[i] >> 4);
-        r[i * 2 + 1] = binary_to_hex_digit(binary[i] & 15);
-    }
-    return r;
-}
+//         output.push_back(((p - lut) << 4) | (q - lut));
+//     }
+//     return output;
+// }
+
+// // uses overloaded '=' operator from string class
+// // to convert character array to string
+// string convertToString(char *a)
+// {
+//     string s = a;
+//     return s;
+// }
+
+// // inline char binary_to_hex_digit(unsigned a)
+// // {
+// //     return a + (a < 10 ? '0' : 'a' - 10);
+// // }
+// // std::string binary_to_hex(unsigned char const *binary, unsigned binary_len)
+// // {
+// //     std::string r(binary_len * 2, '\0');
+// //     for (unsigned i = 0; i < binary_len; ++i)
+// //     {
+// //         r[i * 2] = binary_to_hex_digit(binary[i] >> 4);
+// //         r[i * 2 + 1] = binary_to_hex_digit(binary[i] & 15);
+// //     }
+// //     return r;
+// // }
+
+// string hex_to_binary(const string &s)
+// {
+//     string out;
+//     for (auto i : s)
+//     {
+//         uint8_t n;
+//         if (i <= '9' and i >= '0')
+//             n = i - '0';
+//         else
+//             n = 10 + i - 'A';
+//         for (int8_t j = 3; j >= 0; --j)
+//             out.push_back((n & (1 << j)) ? '1' : '0');
+//     }
+
+//     return out;
+// }
+
+// string binary_to_hex(const string &s)
+// {
+//     string out;
+//     for (uint i = 0; i < s.size(); i += 4)
+//     {
+//         int8_t n = 0;
+//         for (uint j = i; j < i + 4; ++j)
+//         {
+//             n <<= 1;
+//             if (s[j] == '1')
+//                 n |= 1;
+//         }
+
+//         if (n <= 9)
+//             out.push_back('0' + n);
+//         else
+//             out.push_back('A' + n - 10);
+//     }
+
+//     return out;
+// }
